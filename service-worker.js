7afabd6493c272be16a98e3478f129fe62120a7f
Charulatha Mani <charulathamani118@gmail.com>
2023-02-21 09:48:00 +0400

    fg

--- service-worker.js
+++ service-worker.js
@@ -29,17 +29,17 @@ self.addEventListener('install', (e) => {
     )
 })
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then( function (r) {
-            console.log('[Service Worker] Fetch resource' + e.request.url)
+self.addEventListener('fetch', function (e) {  
+    e.respondWith(    
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache, 
             return r || fetch(e.request).then(function (response) {
-                return caches.open(cacheName).then(function (cache) {
-                    cache.put(e.request, response.clone())
-                    return response
-                })
-            })
-        })
-    )
-    
-})
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {          
+                    cache.put(e.request, response.clone());
+                    return response;        
+                });      
+            });    
+        })  
+        );
+    });
